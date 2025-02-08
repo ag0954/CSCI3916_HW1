0aa79df0404b273a41475ed57ce302fa3754e222
ag0954 <gutierabra@gmail.com>
2025-02-07 17:10:33 -0700

    fix error code

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 app.use(express.json());
 
 app.post('/', (req, res)=>{
-    const acceptHeader = req.get('accept');
+    const acceptHeader = req.get('Accept');
     
+    if(acceptHeader){
+        res.setHeader('Accept',acceptHeader);
+    }
+
     const responseBody = {
         acceptHeader: acceptHeader,
         ...req.body
